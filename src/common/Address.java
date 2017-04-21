ce4427b05787fb1ef3d86b134dc8ddecf6b50e29
msanders11 <msanders11@L11713042776.wctc.msft>
2017-04-20 20:59:39 -0500

    initial git

---
+++ src/common/Address.java
@@ -0,0 +1,61 @@
+package common;
+
+public class Address {
+    private String address1;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phone;
+    private String email;
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+}
